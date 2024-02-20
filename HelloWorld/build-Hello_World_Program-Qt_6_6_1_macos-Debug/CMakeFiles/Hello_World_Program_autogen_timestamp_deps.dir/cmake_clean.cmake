22c7c6c8705f469609a59dd28f41983c9d140e0c
Hayk Pipoyan <haykpipoyan@Hayks-MacBook-Air-3.local>
2024-02-20 19:31:01 +0400

    removing build directory

--- HelloWorld/build-Hello_World_Program-Qt_6_6_1_macos-Debug/CMakeFiles/Hello_World_Program_autogen_timestamp_deps.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/Hello_World_Program_autogen_timestamp_deps"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/Hello_World_Program_autogen_timestamp_deps.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- HelloWorld/build-Hello_World_Program-Qt_6_6_1_macos-Debug/CMakeFiles/Hello_World_Program_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for Hello_World_Program_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
